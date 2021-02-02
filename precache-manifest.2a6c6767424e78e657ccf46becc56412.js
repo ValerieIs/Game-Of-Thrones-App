157d25c22f98940aa9b3c09831d4ce70ce67fba0
Valeria Isaeva <valevs.isv@gmail.com>
2021-02-02 17:05:06 +0300

    Updates

---
+++ precache-manifest.2a6c6767424e78e657ccf46becc56412.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a94619240df84f98bca",
+    "url": "/game-of-thrones-app/static/css/main.07884799.chunk.css"
+  },
+  {
+    "revision": "4a94619240df84f98bca",
+    "url": "/game-of-thrones-app/static/js/main.4a946192.chunk.js"
+  },
+  {
+    "revision": "7bd794679d96fecec99c",
+    "url": "/game-of-thrones-app/static/css/1.28d81602.chunk.css"
+  },
+  {
+    "revision": "7bd794679d96fecec99c",
+    "url": "/game-of-thrones-app/static/js/1.7bd79467.chunk.js"
+  },
+  {
+    "revision": "5b3a27f868486157979b",
+    "url": "/game-of-thrones-app/static/js/runtime~main.5b3a27f8.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/game-of-thrones-app/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/game-of-thrones-app/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "9dc0ae0cf05012def6956ee1e3ff0238",
+    "url": "/game-of-thrones-app/index.html"
+  }
+];
