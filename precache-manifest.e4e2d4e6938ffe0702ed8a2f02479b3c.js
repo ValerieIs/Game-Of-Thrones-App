b258e8a6184c5b8409b4bc0bfd29bc44ccd00368
Valeria Isaeva <valevs.isv@gmail.com>
2021-02-02 17:43:47 +0300

    Updates

---
+++ precache-manifest.e4e2d4e6938ffe0702ed8a2f02479b3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "73840db63d38eb913fae",
+    "url": "/Game-Of-Thrones-App/static/css/main.f9e3b102.chunk.css"
+  },
+  {
+    "revision": "73840db63d38eb913fae",
+    "url": "/Game-Of-Thrones-App/static/js/main.73840db6.chunk.js"
+  },
+  {
+    "revision": "7bd794679d96fecec99c",
+    "url": "/Game-Of-Thrones-App/static/css/1.28d81602.chunk.css"
+  },
+  {
+    "revision": "7bd794679d96fecec99c",
+    "url": "/Game-Of-Thrones-App/static/js/1.7bd79467.chunk.js"
+  },
+  {
+    "revision": "509d9a15e7ea1b1daa2b",
+    "url": "/Game-Of-Thrones-App/static/js/runtime~main.509d9a15.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/Game-Of-Thrones-App/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/Game-Of-Thrones-App/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "432c8febafb631a8905df3bc09b469be",
+    "url": "/Game-Of-Thrones-App/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/game-of-thrones-app/precache-manifest.2a6c6767424e78e657ccf46becc56412.js"
+  "/Game-Of-Thrones-App/precache-manifest.e4e2d4e6938ffe0702ed8a2f02479b3c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/game-of-thrones-app/index.html", {
+workbox.routing.registerNavigationRoute("/Game-Of-Thrones-App/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
